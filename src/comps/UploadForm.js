bfb4be4b2983d0d77d6649c1329068a3a3c53774
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-03-17 22:49:35 +0100

    createdinpt hooks

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -5,13 +5,14 @@ const UploadForm = () => {
 
     const [file, setFile] = useState(null);
     const types = ['image/png', 'jpeg'];
-    const [error, setError] = useState('');
+    const [error, setError] = useState(null);
 
     const changeHandler = (e) => {
         let selected = e.target.files[0];
 
         if (selected && types.includes(selected.type)) {
             setFile(selected);
+            setError(null)
         } else {
             setFile(null);
             setError('please select an image file (png pr jpeg)')
@@ -21,6 +22,10 @@ const UploadForm = () => {
   return (
    <form>
        <input type="file" onChange={changeHandler}/>
+       <div className="output">
+           {error && <div className="error">{error}</div>}
+           {file && <div className="file-name">{file.name}</div>}
+       </div>
    </form>
   );
 }
